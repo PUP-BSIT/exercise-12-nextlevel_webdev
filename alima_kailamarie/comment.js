55f1f25d3801db3005f2875d84a847129c85a059
Alima, Kaila Marie <146612718+alima-kailamarie@users.noreply.github.com>
2023-11-08 23:58:26 +0800

    edit the html and js code

--- alima_kailamarie/comment.js
+++ alima_kailamarie/comment.js
@@ -1,40 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Enable the "Comment" button when both fields are filled out
-    const nameInput = document.getElementById("name");
-    const commentInput = document.getElementById("comment");
-    const commentButton = document.getElementById("comment_button");
-    const commentDisplay = document.getElementById("comment_display");
+// Enable the "Comment" button when both fields are filled out
+const nameInput = document.getElementById("name");
+const commentInput = document.getElementById("comment");
+const commentButton = document.getElementById("comment_button");
+const commentDisplay = document.getElementById("comment_Display");
 
-    nameInput.addEventListener("input", toggleCommentButton);
-    commentInput.addEventListener("input", function () {
-        toggleCommentButton();
+nameInput.addEventListener("input", toggleCommentButton);
+commentInput.addEventListener("input", function () {
+    toggleCommentButton();
         commentDisplay.textContent = commentInput.value;
-    });
+});
 
-    function toggleCommentButton() {
-        commentButton.disabled = !(nameInput.value && commentInput.value);
-    }
+function toggleCommentButton() {
+    commentButton.disabled = !(nameInput.value && commentInput.value);
+}
 
-    // Add the event listener for the form submission
-    document.getElementById('comment_form').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the form from submitting and refreshing the page
+document.getElementById('comment_form').addEventListener('submit', 
+function(event) {
+event.preventDefault(); 
 
-        const name = nameInput.value;
-        const comment = commentInput.value;
+const name = nameInput.value;
+const comment = commentInput.value;
 
-        // Create a new comment element
-        const newComment = document.createElement('div');
-        newComment.classList.add('comment');
-        newComment.innerHTML = `<strong>${name}:</strong> ${comment}`;
+const newComment = document.createElement('div');
+newComment.classList.add('comment');
+newComment.innerHTML = `<strong>${name}:</strong> ${comment}`;
 
-        // Get the comment section and insert the new comment at the top
-        commentDisplay.insertBefore(newComment, commentDisplay.firstChild);
+commentDisplay.insertBefore(newComment, commentDisplay.firstChild);
 
-        // Clear the input fields
-        nameInput.value = '';
-        commentInput.value = '';
+nameInput.value = '';
+commentInput.value = '';
 
-        // Disable the comment button after submitting
-        commentButton.disabled = true;
-    });
+commentButton.disabled = true;
+});
 });
